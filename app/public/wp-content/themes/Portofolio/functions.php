2c7b6f76b7c0a0d1e79a7b0602b75d1837fa4040
COBNETCKNN <ahmedmeshkin@gmail.com>
2021-02-07 18:13:31 +0100

    new theme from scratch with laravel mix installed

---
+++ app/public/wp-content/themes/Portofolio/functions.php
@@ -0,0 +1,43 @@
+<?php 
+
+function portofolio_files() {
+
+    //enqueing CSS
+        wp_enqueue_style('mainCSS', get_template_directory_uri() . '/css/main.css');
+
+
+    //enqueing JS
+        wp_enqueue_script('mainJS', get_stylesheet_directory_uri() . '/js/main.js', array(), 1.0, true);
+
+}
+add_action('wp_enqueue_scripts', 'portofolio_files')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
